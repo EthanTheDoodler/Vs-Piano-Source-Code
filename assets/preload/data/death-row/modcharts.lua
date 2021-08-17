6acd5e416f5a16215bf54f774adf471e47a9d102
EthanTheDoodler <83174242+EthanTheDoodler@users.noreply.github.com>
2021-08-17 19:41:00 -0400

    MY TESTICLES

---
+++ assets/preload/data/death-row/modcharts.lua
@@ -0,0 +1,22 @@
+function start (song)
+	print("Succesfully initizlized our song (death-row)")
+end
+
+function update (elapsed)
+
+end
+
+function beatHit (beat)
+	print("da beat has hit")
+
+end
+
+function stepHit (beat)
+	if curStep == 81 then
+		showOnlyStrums = true
+	end
+end
+
+function keyPressed (key)
+
+end
